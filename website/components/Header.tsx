6bbc60594fc63e8175b4b3cc3b0fcb04dc74d2f6
bcortois <bertwcpot@gmail.com>
2026-01-14 22:50:25 +0100

    Update Header - test border

--- website/components/Header.tsx
+++ website/components/Header.tsx
@@ -27,7 +27,7 @@ const Header = () => {
           )}
         </div>
       </Link>
-      <div className="flex items-center space-x-0 leading-5 sm:-mr-6 sm:space-x-6">
+      <div className="flex items-center space-x-0 leading-5 sm:-mr-6 sm:space-x-6 border border-solid border-indigo-600">
         <div className="no-scrollbar hidden max-w-40 items-center gap-x-4 overflow-x-auto sm:flex md:max-w-72 lg:max-w-96 border-solid">
           {headerNavLinks
             .filter((link) => link.href !== '/')
