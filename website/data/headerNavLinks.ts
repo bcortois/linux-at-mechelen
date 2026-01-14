81cfe291a2a1441dd7e9289f4209bfaa93dae7b6
bcortois <bertwcpot@gmail.com>
2026-01-14 21:22:04 +0100

    Update headerNavLinks - Kennishub test
    Added dummylink for KennisHub.

--- website/data/headerNavLinks.ts
+++ website/data/headerNavLinks.ts
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/exercises', title: 'Oefeningen' },
   { href: '/events', title: 'Events' },
+  { href: 'https://linux-at-kortijk.be', title: 'Kennishub' },
   { href: '/about', title: 'Over ons' },
 ]
 
