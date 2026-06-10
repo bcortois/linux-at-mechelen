2dddb4b5b795b6cee11003693e6694ae785c9527
bcortois <bertwcpot@gmail.com>
2026-06-10 21:38:50 +0200

    Update page.tsx

--- website/app/events/page.tsx
+++ website/app/events/page.tsx
@@ -126,7 +126,7 @@ export default function Events() {
           <li>Reservatie: Mail naar <a href="mailto:info@linux-at-mechelen.be">info@linux-at-mechelen.be</a></li>
         </ul>
       </div>
-      <div className="prose dark:prose-invert max-w-none pt-4">
+      <div className="prose dark:prose-invert max-w-none pt-4" hidden>
         <h1 className="text-lg leading-9 font-extrabold tracking-tight text-gray-900 dark:text-gray-100">
           Installatiesessie @ Recupperij
         </h1>
